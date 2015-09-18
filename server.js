7827efe5ec78bdac16389361f2a11c6d82ed371e
yoonil63 <yoonil63@gmail.com>
2015-09-18 07:37:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.post('/update', function(req, res) {
+    console.log('test ' + req);
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
